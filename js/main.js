4eb8d61dd791758ba38c9833a4b3b7b55ff16b18
Dave Johnson <gearhead@fullsail.edu>
2012-09-08 09:28:00 -0400

    Updated js and removed option and select tags in html.

--- js/main.js
+++ js/main.js
@@ -26,10 +26,11 @@ window.addEventListener("DOMContentLoaded", function(){
     }
     
     //Variable defaults
-    var state = ["--Choose a State--",
+    var state = [
+                 "--Choose a State--",
                  "AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO",
                  "MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"
-                 ];
+                ];
     chooseState();
     
     
